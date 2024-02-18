099bc0cbf39ff4a7e5c41183b7c26ccd3e0e2d25
LeweLotki <stefanczyk42@gmail.com>
2024-02-18 18:22:59 +0100

    stereovision first try

--- calibration_images.py
+++ calibration_images.py
@@ -3,21 +3,19 @@ import os
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-cap2 = cv2.VideoCapture(1)
 
 num = 0
 
 while cap.isOpened():
 
-    succes1, img = cap.read()
-    succes2, img2 = cap2.read()
+    succes, img = cap.read()
 
-    img2_size = img2.shape
+    img_size = img.shape
     
-    imgL = np.zeros((img2_size[0], int(img2_size[1] / 2), img2_size[2]))
-    imgR = np.zeros((img2_size[0], int(img2_size[1] / 2), img2_size[2]))
-    imgL = img2[:,:int(img2_size[1]/2),:]
-    imgR = img2[:,int(img2_size[1]/2) :,:]
+    imgL = np.zeros((img_size[0], int(img_size[1] / 2), img_size[2]))
+    imgR = np.zeros((img_size[0], int(img_size[1] / 2), img_size[2]))
+    imgL = img[:,:int(img_size[1]/2),:]
+    imgR = img[:,int(img_size[1]/2) :,:]
 
     k = cv2.waitKey(5)
 
@@ -33,13 +31,10 @@ while cap.isOpened():
         print(os.getcwd())
         num += 1
 
-    print(img2_size)
-    
     cv2.imshow('Img 1',imgL)
     cv2.imshow('Img 2',imgR)
 
 # Release and destroy all windows before termination
 cap.release()
-cap2.release()
 
 cv2.destroyAllWindows()
