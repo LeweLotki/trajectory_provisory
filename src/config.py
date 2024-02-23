01e19d1cd09d6dbe8908f862a1b492d1ae01b521
LeweLotki <stefanczyk42@gmail.com>
2024-02-23 22:15:42 +0100

    creating main

--- src/config.py
+++ src/config.py
@@ -3,3 +3,6 @@ from ypstruct import struct
 paths = struct()
 
 paths.description_path = './attributes/description.txt' 
+paths.calibration_description_path = './attributes/calibration_description.txt' 
+paths.stream_description_path = './attributes/stream_description.txt' 
+paths.stereo_description_path = './attributes/stereo_description.txt' 
--- src/main.py
+++ src/main.py
@@ -7,43 +7,55 @@ from os.path import basename
 
 def main():
 
-    program_description = get_description(path=paths.description_path)
-    
-    data_analysis_description = get_description(path=paths.data_analysis_description_path)
-
-    parser = ArgumentParser(description=program_description)
-    
-    parser.add_argument('-a', '--notebook', help=data_analysis_description + f' {", ".join(available_notebooks)}')
-    
-    parser.add_argument('-e', action='store_true', help=data_extraction_description)
-
-    args = parser.parse_args()
-    
-    if args.notebook: run_analysis(args.notebook)
-    elif args.e: run_extraction()
-    elif args.p: 
-        if args.sampling_coef:
-            cost_function(sampling_coef=args.sampling_coef)
-        else: cost_function()
-    elif args.s: 
-        pso = PSO(
-            file_path=args.cost_function_file_path,
-            options=args.weights,
-            n_particles=args.n_particles,
-            iters=args.iters
-        )
-        pso.train()
-        pso.display(fps=10)
-    else: default_message()
+    parser = get_parser()
+    select_mode(parser=parser)
 
 def default_message():
     
     print("\r No option specified. \n\r Type '--help' for list of arguments.")
 
-def get_description(path: str) -> str:
+def open_textfile(path: str) -> str:
     
     with open(path, 'r', encoding='utf-8') as file:
         description = file.read().strip()
         return description
- 
+
+def get_descriptions() -> list:
+
+    return [
+        open_textfile(path=paths.description_path),
+        open_textfile(path=paths.calibration_description_path),
+        open_textfile(path=paths.stream_description_path),
+        open_textfile(path=paths.stereo_description_path)
+        ]
+
+def get_parser():
+    
+    (
+        description_path,
+        calibration_description_path,
+        stream_description_path,
+        stereo_description_path
+    ) = get_descriptions() 
+
+    parser = ArgumentParser(description=description_path)
+    parser.add_argument('-c','--calibration', action='store_true', help=calibration_description_path)
+    parser.add_argument('-s','--stream', action='store_true', help=stream_description_path)
+    parser.add_argument('-d','--stereo', action='store_true', help=stereo_description_path)
+
+    return parser
+
+def select_mode(parser):
+    # TODO 
+    args = parser.parse_args()
+    
+    if args.calibration: 
+        print('calibration mode')
+    elif args.stream: 
+        print('stream mode')
+    elif args.stereo: 
+        print('stereo mode')
+    else: default_message()
+
 if __name__ == '__main__': main()
+
